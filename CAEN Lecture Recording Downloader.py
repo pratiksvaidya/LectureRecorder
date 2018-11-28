aa7b997348c885e65528c9b2ebfee9a07eef1bc0
= <pratik@prahan.com>
2018-11-28 15:33:04 -0500

    validate inputs

--- CAEN Lecture Recording Downloader.py
+++ CAEN Lecture Recording Downloader.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import getpass
 import selenium.webdriver as webdriver
+import sys
 import time
 import urllib.request
 
@@ -12,6 +13,20 @@ url = 'https://leccap.engin.umich.edu/leccap/'
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get(url)
+
+def validate_num_input(list_size, input):
+    if input == '':
+        return False
+    elif input.lower() == 'quit':
+        sys.exit()
+
+    try:
+        val = int(input)
+        if val <= 0 or val > list_size:
+            return False
+        return True
+    except ValueError:
+        return False
 
 # Login
 print('Enter UM username: ', end = '')
@@ -41,15 +56,21 @@ for i in range(len(classes)):
     print(str(i+1) + ") " + classes[i].text)
     
 print('\nSelect course from the list above: ', end = '')
-course_num = int(input())
+course_num = input()
+while not (validate_num_input(len(classes), course_num)):
+    print('Your input is invalid. Please enter a number between 1 and ' + str(len(classes)) + ': ' , end = '')
+    course_num = input()
 
-course_url = 'https://leccap.engin.umich.edu/' + classes[course_num-1]['href']
+course_url = 'https://leccap.engin.umich.edu/' + classes[int(course_num)-1]['href']
 
 driver.get(course_url)
 
 # Get Lectures
 soup = BeautifulSoup(driver.page_source, features='html.parser')
 classes = soup.find('div', id="recordings").findAll('a')
+if len(classes) == 0:
+    print('There are no lectures recorded for this class.')
+    sys.exit()
 
 # Select Lecture
 soup = BeautifulSoup(driver.page_source, features='html.parser')
@@ -59,9 +80,12 @@ for i in range(len(lectures)):
     print (str(i+1) + ") " + lectures[i]['title'][6:].strip())
 
 print('\nSelect lecture from the list above: ', end = '')
-lecture_num = int(input())
+lecture_num = input()
+while not (validate_num_input(len(lectures), lecture_num)):
+    print('Your input is invalid. Please enter a number between 1 and ' + str(len(lectures)) + ': ' , end = '')
+    lecture_num = input()
 
-lecture_url = 'https://leccap.engin.umich.edu' + lectures[lecture_num-1]['href']
+lecture_url = 'https://leccap.engin.umich.edu' + lectures[int(lecture_num)-1]['href']
 
 driver.get(lecture_url)
 
